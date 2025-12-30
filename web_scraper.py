649d93681f156fa2bcaba626a3770ddbff2a64a5
katGhost <katghost124@gmail.com>
2025-12-30 15:27:12 +0200

    Save file to demos folder

--- web_scraper.py
+++ web_scraper.py
@@ -16,4 +16,21 @@ url = "https://www.passiton.com/inspirational-quotes"
 
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
-print(soup.prettify())
+#print(soup.prettify())
+with open("./files/demos/demo1.txt", "w", encoding='utf-8') as f:
+    f.write(soup.prettify())
+
+"""quotes = []
+quote_boxes = soup.find_all('div', class_="text-center mb-8")
+for box in quote_boxes:
+    quote_text = box.img['alt'].split(" #") #type: ignore
+    quote = {
+        'theme': box.h5.text.strip(),   #type: ignore
+        'image_url': box.img['src'],    #type: ignore
+        'lines': quote_text[0],
+        'author': quote_text[1] if len(quote_text) > 1 else 'Unknown'
+    }
+
+    quotes.append(quote)"""
+
+
