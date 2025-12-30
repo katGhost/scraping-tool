be9ced353896b80d623c19c96cb37b6a0051753f
katGhost <katghost124@gmail.com>
2025-12-30 15:39:20 +0200

    Add scraped data to csv file

--- web_scraper.py
+++ web_scraper.py
@@ -1,4 +1,4 @@
-import os
+import os, csv, json
 import requests
 from bs4 import BeautifulSoup
 
@@ -17,10 +17,8 @@ url = "https://www.passiton.com/inspirational-quotes"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 #print(soup.prettify())
-with open("./files/demos/demo1.txt", "w", encoding='utf-8') as f:
-    f.write(soup.prettify())
 
-"""quotes = []
+quotes = []
 quote_boxes = soup.find_all('div', class_="text-center mb-8")
 for box in quote_boxes:
     quote_text = box.img['alt'].split(" #") #type: ignore
@@ -31,6 +29,25 @@ for box in quote_boxes:
         'author': quote_text[1] if len(quote_text) > 1 else 'Unknown'
     }
 
-    quotes.append(quote)"""
+    quotes.append(quote)
+
+#Save response to a csv file, more dict readable
+# slice to get the required amount/limit of quotes
+# q is a dict -> convert it to str or json to avoid
+# TypeError: write() must be str not dict
+# Save it as a CSV
+filename = "quotes.csv"
+with open(f"./files/scraped_essentials/{filename}", mode='w', newline='', encoding='utf-8') as f:
+    writer = csv.DictWriter(f, fieldnames=['theme', 'image_url', 'lines', 'author'])
+    writer.writeheader()
+    for q in quotes:
+        writer.writerow(q)
 
 
+#Save response to a txt file | Create a directory
+"""with open("./files/demos/demo1.txt", "w", encoding='utf-8') as f:
+    f.write(soup.prettify())"""
+
+# read from the file
+"""with open("demo1.txt") as f:
+    print(f.read())"""
